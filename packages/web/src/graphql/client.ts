e09110ca362f3e518607bcf761941588d247bfe4
Fernando Salas <nanddosalas@gmail.com>
2022-01-09 10:29:48 -0700

    add apollo client context

--- packages/web/src/graphql/client.ts
+++ packages/web/src/graphql/client.ts
@@ -1,6 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Cookies from 'js-cookie';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const accessToken = Cookies.get('accessToken');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: accessToken ? `Bearer ${accessToken}` : '',
+    },
+  };
+});
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
