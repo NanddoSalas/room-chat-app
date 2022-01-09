b1a07da648682e492b6d2708e029ce2704e4cfc7
Fernando Salas <nanddosalas@gmail.com>
2022-01-08 20:04:04 -0700

    setup apollo client

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,7 +1,9 @@
+import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider, extendTheme, ThemeConfig } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { client } from './graphql/client';
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
@@ -12,9 +14,11 @@ const theme = extendTheme({ config });
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
