9099bbb0f78ae39db42cfe3d08cf54cb40493faa
Fernando Salas <nanddosalas@gmail.com>
2022-01-11 19:20:28 -0700

    update MainForm

--- packages/web/src/components/MainForm.tsx
+++ packages/web/src/components/MainForm.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Input, VStack } from '@chakra-ui/react';
+import { Button, HStack, Input, useToast, VStack } from '@chakra-ui/react';
 import { useState } from 'react';
 import {
   useCreateRoomMutation,
@@ -8,15 +8,51 @@ import {
 const MainForm = () => {
   const [roomName, setRoomName] = useState('');
   const [invitationCode, setInvitationCode] = useState('');
-  const [, createRoom] = useCreateRoomMutation();
-  const [, joinRoom] = useJoinRoomMutation();
+  const [{ fetching: loadingCreateRoom }, createRoom] = useCreateRoomMutation();
+  const [{ fetching: loadingJoinRoom }, joinRoom] = useJoinRoomMutation();
+  const toast = useToast();
 
   const handleCreateRoom = async () => {
-    createRoom({ name: roomName });
+    if (!roomName) return;
+
+    setRoomName('');
+    const { data } = await createRoom({ name: roomName });
+    if (data?.newRoom) {
+      toast({
+        title: `The room ${data.newRoom.name} has been created.`,
+        description: `The invitation code is ${data.newRoom.invitationCode}.`,
+        status: 'success',
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: 'Something went wrog.',
+        description: `The room has not being created.`,
+        status: 'error',
+        isClosable: true,
+      });
+    }
   };
 
   const handleJoinRoom = async () => {
-    joinRoom({ invitationCode });
+    if (!invitationCode) return;
+
+    setInvitationCode('');
+    const { data } = await joinRoom({ invitationCode });
+    if (data?.room) {
+      toast({
+        title: `You've joined to ${data.room.name}.`,
+        description: 'Connect to the room to start sending messages.',
+        status: 'success',
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: 'Invalid invitation code.',
+        status: 'error',
+        isClosable: true,
+      });
+    }
   };
 
   return (
@@ -26,8 +62,16 @@ const MainForm = () => {
           placeholder="Room Name"
           value={roomName}
           onChange={({ target }) => setRoomName(target.value)}
+          isDisabled={loadingCreateRoom}
+          required
         />
-        <Button colorScheme="blue" width="2xs" onClick={handleCreateRoom}>
+        <Button
+          colorScheme="blue"
+          width="2xs"
+          onClick={handleCreateRoom}
+          isDisabled={loadingCreateRoom}
+          isLoading={loadingCreateRoom}
+        >
           Create Room
         </Button>
       </HStack>
@@ -37,12 +81,15 @@ const MainForm = () => {
           placeholder="Invitation Code"
           value={invitationCode}
           onChange={({ target }) => setInvitationCode(target.value)}
+          isDisabled={loadingJoinRoom}
         />
         <Button
           colorScheme="blue"
           variant="outline"
           width="2xs"
           onClick={handleJoinRoom}
+          isDisabled={loadingJoinRoom}
+          isLoading={loadingJoinRoom}
         >
           Join Room
         </Button>
